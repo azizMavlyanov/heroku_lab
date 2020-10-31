c06056f35d974334ef54d5951beb4bfa9786f2b0
azizMavlyanov <alex.mavlyanov95@gmail.com>
2020-10-31 22:18:43 +0300

    Changed implementation

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ const CORS = {
 res.writeHead(200, CORS);
 if (req.url === '/login/') return res.end('alexmavlyanov95');
 if (req.url === '/login') return res.end('alexmavlyanov95');
-if (req.url === '/sample/') return res.end(function task(x) {
-    return x * this * this;
-});
-if (req.url === '/sample') return res.end(function task(x) {
-    return x * this * this;
-});
+if (req.url === '/sample/') return res.end('function task(x) { return x * this * this;}');
+if (req.url === '/sample') return res.end('function task(x) { return x * this * this;}');
+
 res.end('test');
 })
 .listen(process.env.PORT);
