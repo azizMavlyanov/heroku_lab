e7e7738660a038c7228da0988a614e1270d13824
azizMavlyanov <alex.mavlyanov95@gmail.com>
2020-11-01 14:31:38 +0300

    Fix

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ const CORS = {
 res.writeHead(200, CORS);
 if (req.url === '/login/') return res.end('alexmavlyanov95');
 if (req.url === '/login') return res.end('alexmavlyanov95');
-if (req.url === '/sample/') return res.end('\nfunction task(x) {\n  return x * this ** 2;\n}');
-if (req.url === '/sample') return res.end('\nfunction task(x) {\n  return x * this ** 2;\n}');
+// if (req.url === '/promise/') return res.end('\nfunction task(x) {\n  return x * this ** 2;\n}');
+if (req.url === '/promise/') return res.end('function task(x){ return new Promise(function(resolve, reject) { if (x < 18) {resolve("yes")} else {reject("no")}});}');
+if (req.url === '/promise') return res.end('function task(x){ return new Promise(function(resolve, reject) { if (x < 18) {resolve("yes")} else {reject("no")}});}');
+
+if (req.url === '/fetch/' || req.url === '/fetch') {
+    
+    const CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'text/html; charset=UTF-8'
+    };
+
+    return res.end(`<input type="text" id="inp"><button id="bt" onclick="fetch(document.getElementById('inp').value).then(response1 => response1.text()).then(response2 => document.getElementById('inp').value = response2)">submit</button>`);
+}
 
 res.end('test');
 })
 .listen(process.env.PORT);
+
+// fetch(`${document.getElementById("inp").value}`).then(response1 => response1.text()).then(response2 => document.getElementById("inp").value = response2);
