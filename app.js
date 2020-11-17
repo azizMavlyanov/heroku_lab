a92e0d9df34ee833dbed639617cb11e21144eb08
azizMavlyanov <alex.mavlyanov95@gmail.com>
2020-11-17 14:06:47 +0300

    Fix code

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ export default function(express, bodyParser, fs, crypto, http) {
     app.get('/sha1/:input/', (req, res) => {
         let {input} = req.params;
         res.setHeader('content-type', 'text/plain');
-        res.send(crypto.createHash('sha1').update(input).digest('hex'));
+        res.send(crypto.createHash('sha1').update(input).digest());
     });
 
     app.get('/req', (req, res) => {
