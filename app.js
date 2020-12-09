fa138cc27df5f9c5d26e45d4f0f77f4433118c58
azizMavlyanov <alex.mavlyanov95@gmail.com>
2020-12-09 18:25:50 +0300

    week9 fix code

--- app.js
+++ app.js
@@ -19,22 +19,22 @@ export default (express, bodyParser, fs, crypto, http, mongodb, path, cors, pupp
     app 
         .get('/test/', async (req, res) => {
             const { URL } = req.query;
-            // const browser = await puppeteer.launch({headless: true, args:['--no-sandbox', '--disable-setuid-sandbox']});
-            // const page = await browser.newPage();
+            const browser = await puppeteer.launch({executablePath: './node_modules/puppeteer/.local-chromium/linux-818858/chrome-linux/chrome', headless: true, args:['--no-sandbox', '--disable-setuid-sandbox']});
+            const page = await browser.newPage();
 
-            // await page.goto(URL);
-            // await page.waitForSelector('#inp');
-            // await page.waitForSelector('#bt');
-            // await page.click('#bt');
+            await page.goto(URL);
+            await page.waitForSelector('#inp');
+            await page.waitForSelector('#bt');
+            await page.click('#bt');
 
-            // const gotResponse = await page.$eval('#inp', el => el.value);
+            const gotResponse = await page.$eval('#inp', el => el.value);
             
-            // browser.close();
+            browser.close();
 
-            // res.send(gotResponse);
+            res.send(gotResponse);
             
-            res.setHeader('content-type', 'text/plain');
-            res.send("0.8862481722945399")
+            // res.setHeader('content-type', 'text/plain');
+            // res.send("0.8862481722945399");
         })
         .get('/wordpress/wp-json/wp/v2/posts/1', (req, res) => res.status(200).json({title: {id: 1, rendered: "alexmavlyanov95"}}))
         .post('/render/', (req, res) => {
